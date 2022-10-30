60b22b08393d5ab6a6dedc62dced8d59a8fb702b
adityarahi <95748245+adityarahi@users.noreply.github.com>
2022-10-30 21:47:32 +0530

    Add files via upload

--- assignment.py
+++ assignment.py
@@ -1,7 +1,7 @@
 # def make_order(order):
 import pandas as pd
 
-pd.read_csv('INPUT FILE')
+df=pd.read_csv('D:/Fynstack/Merging_org/orders.csv')
 
 database={}  #store list of books and each book has 2 dictionarues with key values buy and sell
 
@@ -38,7 +38,7 @@ def order_ADDING(order):
                 if K[0].volume>order_vol:
                     K[0].volume=K[0].volume-order_vol
                 else:
-                    order.dlt(K[0])
+                    order_dlt(K[0])
                     order.volume= order_vol-K[0].volume
                     order_ADDING(order)
         except:
@@ -47,3 +47,17 @@ def order_ADDING(order):
 
 
 def order_dlt(order):
+    order_book=order.book
+    order_op=order.operation
+    order_price=order.price
+    order_vol=order.volume
+    order_id=order.orderID
+    if order_op=='SELL':
+        try:
+            K=database[order_book]['SELL']
+            for i in range(K):
+                if K[i].orderID==order_id:
+                    del K[i]
+                    break
+        except:
+            pass  
